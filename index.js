ba1c43dbc36052bdcf9885ff8e3c74b7923aac1e
jaisondza <jaisonj1010@gmail.com>
2020-04-10 20:08:38 +0530

    modifications

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var scraper = new Scraper('https://www.flipkart.com/');
 // scraper.scrape(function(image) { 
 //     image.save();
 // });
-let search_item= 'paint';
+let search_item= 'gifts';
 
 nightmare.goto(url)
 .wait('body')
@@ -33,10 +33,16 @@ let getData = html=>{
     $('div._1HmYoV._35HD7C:nth-child(2) div.bhgxx2.col-12-12').each((row, raw_elem)=>{
         $(raw_elem).find('div div div').each((i, elem)=>{
             
-            let title= $(elem).find('div div a:nth-child(2)').text();
+            // let title= $(elem).find('div div a:nth-child(2)').text();
             // let link= $(elem).find('div div a:nth-child(2)').attr('href');
-            let link= $(elem).find('img._1Nyybr').attr('src');
-            let price= $(elem).find('div._1vC4OE').text().substring();
+            
+            // let title= $(elem).find('a._2cLu-l').text();
+            // let link= $(elem).find('img._1Nyybr').attr('src');
+            // let price= $(elem).find('div._1vC4OE').text().substring();
+
+            let link= $(elem).find('a._31qSD5 img._1Nyybr').attr('src');
+            let title= $(elem).find('div._1-2Iqu div div._3wU53n').text();
+            let price= $(elem).find('div._1-2Iqu div:nth-child(2) div div div._1vC4OE').text().substring();
             if(title){
                 data.push({
                     
